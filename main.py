eb32726e13266ea893753d33806a819a1049d1ff
DaniilKarn <ank.t@mail.ru>
2023-12-18 20:55:07 +0300

    ADD DATABASE

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort, g
+from FDataBase import FDataBase
 
 # config
 DATABASE = "/tmp/flsite.db"
@@ -13,10 +14,6 @@ app.config.from_object(__name__)
 # переопределим путь к базе данных
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
-
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -25,6 +22,7 @@ def connect_db():
 
 
 def create_db():
+    '''Вспомогаательная функця для создания таблиц БД'''
     db = connect_db()
     with app.open_resource('sq_db.sql', mode='r') as f:
         db.cursor().executescript(f.read())
@@ -32,14 +30,57 @@ def create_db():
         db.close()
 
 
+def get_db():
+    '''Соединение с БДБ если оно еще не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+def get_menu():
+    db = get_db()
+    dbase = FDataBase(db)
+    return dbase.getMenu()
+
+
 @app.route("/")
 def index():
-    return render_template('index.html', title="About Flask", menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', posts=dbase.getPostsAnnonce())
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html', menu=menu)
+@app.route("/add_post", methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'] )
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+
+    return render_template('add_post.html', title='Добавление статьи')
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', title=title, post=post)
 
 
 @app.route('/contact', methods=['POST', 'GET'])
@@ -49,7 +90,7 @@ def contact():
             flash('Сообщение отправлено', category='success')
         else:
             flash('Ошибка отправки', category='error')
-    return render_template('contacts.html', menu=menu, title='Our contacts')
+    return render_template('contacts.html', title='Our contacts')
 
 
 @app.route("/profile/<username>")
@@ -63,18 +104,18 @@ def profile(username):
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
+        return redirect(url_for('index', username=session['userLogged']))
     elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
         session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
+        return redirect(url_for('index', username=session['userLogged']))
 
-    return render_template('login.html', title="Авторизация", menu=menu)
+    return render_template('login.html', title="Авторизация")
 
 
 @app.errorhandler(404)
 def pageNotFount(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu)
+    return render_template('page404.html', title="Страница не найдена")
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
